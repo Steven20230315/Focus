033a5b2ac8339c72f99bd2f73161af94bf79f2d3
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-22 17:51:42 +0800

    Refactor: Rename ProjectList to List in Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import ProjectList from "../features/list/List";
+import List from "../features/list/List";
 import CreateList from "../features/list/CreateList";
 
 const Container = styled.div``;
@@ -7,7 +7,7 @@ const Container = styled.div``;
 export default function Sidebar() {
   return (
     <Container className="flex flex-col gap-2 bg-slate-100 p-2">
-      <ProjectList />
+      <List />
       <CreateList />
     </Container>
   );
