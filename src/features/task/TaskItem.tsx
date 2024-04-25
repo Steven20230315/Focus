21aeaacda0f4636012e863fd169b77fa8ffabf1d
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-25 17:21:20 +0800

    Refactor TaskItem and TaskList for Better Layout
    - Introduce a grid-based layout in TaskItem for structured display of
    task properties

--- src/features/task/TaskItem.tsx
+++ src/features/task/TaskItem.tsx
@@ -1,23 +1,17 @@
 import { TaskId } from '../../types';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store';
-// import { AiOutlineCheckCircle } from 'react-icons/ai';
-import {
-  Draggable,
-  type DraggableProvided,
-} from '@hello-pangea/dnd';
+import TaskTitle from './TaskTitle';
+import { Draggable, type DraggableProvided } from '@hello-pangea/dnd';
+import { AiOutlineSmallDash } from 'react-icons/ai';
+
 type TaskItemProps = {
   taskId: TaskId;
   index: number;
 };
 
-export default function TaskItem({
-  taskId,
-  index,
-}: TaskItemProps) {
-  const task = useSelector(
-    (state: RootState) => state.task.allTasks[taskId],
-  );
+export default function TaskItem({ taskId, index }: TaskItemProps) {
+  const task = useSelector((state: RootState) => state.task.allTasks[taskId]);
   return (
     <Draggable draggableId={taskId} index={index}>
       {(provided: DraggableProvided) => (
@@ -25,15 +19,20 @@ export default function TaskItem({
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          className=":hover:bg-black flex gap-5 bg-slate-100 p-2"
+          className="grid h-full grid-cols-12 gap-4 pb-3 pt-1 text-sm"
         >
-          <p>
-            {/* <AiOutlineCheckCircle/> */}
-            <span className="m-2 font-semibold">
-              {index + 1}.
-            </span>
-            {task.title}
-          </p>
+          {/* <AiOutlineCheckCircle/> */}
+          <div className="col-span-5">
+            <TaskTitle id={taskId} title={task.title} />
+          </div>
+          <div className="col-span-7 col-start-6 grid grid-cols-6 place-items-center gap-6 text-center">
+            <div className="col-span-2">Time Spend</div>
+            <div className="col-span-2">Due Date</div>
+            <div className="col-span-1">Priority</div>
+            <div>
+              <AiOutlineSmallDash className="m-auto" />
+            </div>
+          </div>
         </div>
       )}
     </Draggable>
--- src/features/task/TaskList.tsx
+++ src/features/task/TaskList.tsx
@@ -4,6 +4,7 @@ import TaskItem from './TaskItem';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store';
 import { Droppable, type DroppableProvided } from '@hello-pangea/dnd';
+import { FiPlusCircle } from 'react-icons/fi';
 
 type TaskListProps = React.HTMLAttributes<HTMLDivElement> & {
   columnId: ColumnId;
@@ -14,15 +15,34 @@ export default function TaskList({ columnId }: TaskListProps) {
   const tasksInColumn = useSelector((state: RootState) => state.column.allColumns[columnId].taskIds);
 
   return (
-    <Droppable droppableId={columnId} type="task">
-      {(provided: DroppableProvided) => (
-        <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-3">
-          {tasksInColumn.map((taskId: TaskId, index: number) => (
-            <TaskItem key={taskId} taskId={taskId} index={index} />
-          ))}
-          {provided.placeholder}
+    <>
+      <div className="divide-y-2 ">
+        <div className=" grid grid-cols-12 gap-4 py-2 text-start text-lg font-bold">
+          <div className="col-span-5">name</div>
+          <div className="col-span-7 col-start-6 grid grid-cols-6 place-items-center gap-6 text-center">
+            <div className="col-span-2">Time Spend</div>
+            <div className="col-span-2">Due Date</div>
+            <div className="col-span-1">Priority</div>
+            <div>
+              <FiPlusCircle />
+            </div>
+          </div>
         </div>
-      )}
-    </Droppable>
+        <Droppable droppableId={columnId} type="task">
+          {(provided: DroppableProvided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="mb-3 flex flex-col justify-center  divide-y"
+            >
+              {tasksInColumn.map((taskId: TaskId, index: number) => (
+                <TaskItem key={taskId} taskId={taskId} index={index} />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </>
   );
 }
