f2fd7c1ba4386578bda6d8d4ef3a4805162ae4ed
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-26 21:09:08 +0800

    fix:  Update current list when deleting list
    If the list being deleted is the current list, set current list to the
    first list in `listsOrder`.

--- src/features/list/listSlice.ts
+++ src/features/list/listSlice.ts
@@ -1,7 +1,4 @@
-import {
-  createSlice,
-  PayloadAction,
-} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { type List, type ListId } from '../../types';
 import { type DropResult } from '@hello-pangea/dnd';
 // Project slice is responsible for managing projects (CRUD operations)
@@ -77,10 +74,7 @@ const projectSlice = createSlice({
   name: 'project',
   initialState,
   reducers: {
-    addListWithDefaultColumns: (
-      state: ListsState,
-      action: PayloadAction<List>,
-    ) => {
+    addListWithDefaultColumns: (state: ListsState, action: PayloadAction<List>) => {
       // When a new project is created, it has four default columns
       // Type Project has a columnIds field. This field is an array of columnIds. It will be used to create default columns in columnSlice(extraReducers)
       const newList = action.payload;
@@ -88,51 +82,35 @@ const projectSlice = createSlice({
       state.allLists[newListId] = newList;
       state.listsOrder.push(newList.listId);
     },
-    deleteList: (
-      state: ListsState,
-      action: PayloadAction<List>,
-    ) => {
+    deleteList: (state: ListsState, action: PayloadAction<List>) => {
       const listId = action.payload.listId;
       if (!(listId in state.allLists)) {
-        throw new Error(
-          'The list you want to delete does not exist',
-        );
-      } else {
-        delete state.allLists[listId];
+        throw new Error('The list you want to delete does not exist');
       }
+      state.listsOrder = state.listsOrder.filter((listId) => listId !== action.payload.listId);
+      // If the deleted list is the current list, change the current list to the first list. Otherwise, the app will crash.
+      if (state.currentListId === listId) {
+        state.currentListId = state.listsOrder[0];
+      }
+      delete state.allLists[listId];
     },
-    updateCurrentList: (
-      state: ListsState,
-      action: PayloadAction<List>,
-    ) => {
+    updateCurrentList: (state: ListsState, action: PayloadAction<List>) => {
       if (!(action.payload.listId in state.allLists)) {
-        throw new Error(
-          'The project you want to update does not exist',
-        );
+        throw new Error('The project you want to update does not exist');
       } else {
-        state.allLists[action.payload.listId] =
-          action.payload;
+        state.allLists[action.payload.listId] = action.payload;
       }
     },
     // ******************************************************************************************************
     // TODO: Add more comment here. Below is code for managing how lists are displayed
     // ******************************************************************************************************
-    setCurrentList: (
-      state: ListsState,
-      action: PayloadAction<ListId>,
-    ) => {
+    setCurrentList: (state: ListsState, action: PayloadAction<ListId>) => {
       state.currentListId = action.payload;
     },
-    updateListsOrder: (
-      state: ListsState,
-      action: PayloadAction<DropResult>,
-    ) => {
-      const { source, destination, draggableId } =
-        action.payload;
+    updateListsOrder: (state: ListsState, action: PayloadAction<DropResult>) => {
+      const { source, destination, draggableId } = action.payload;
       if (!(draggableId in state.allLists)) {
-        throw new Error(
-          'The list you want to move does not exist',
-        );
+        throw new Error('The list you want to move does not exist');
       }
       if (!destination) {
         console.log('No destination');
@@ -149,19 +127,13 @@ const projectSlice = createSlice({
         console.log('List reorder success');
       } else {
         console.log('List reorder failed');
-        console.log(
-          'Unexpected case. Need to figure out what I missed here',
-        );
+        console.log('Unexpected case. Need to figure out what I missed here');
       }
     },
-    updateColumnsOrderInList: (
-      state: ListsState,
-      action: PayloadAction<DropResult>,
-    ) => {
+    updateColumnsOrderInList: (state: ListsState, action: PayloadAction<DropResult>) => {
       // onDragEnd function ensure 1. source, destination and draggableId are provided 2. source.droppableId and destination.droppableId are the same
       // What onDragEnd does not ensure is droppableId is valid in allLists, and the draggableId is in allLists[source.droppableId].columnIds
-      const { source, destination, draggableId } =
-        action.payload;
+      const { source, destination, draggableId } = action.payload;
       const sourceListId = source.droppableId;
       if (!(sourceListId in state.allLists)) {
         throw new Error(
@@ -169,25 +141,15 @@ const projectSlice = createSlice({
         );
       }
       // Check if the draggableId is in the columnIds of the sourceList
-      if (
-        !state.allLists[
-          sourceListId
-        ].columnIdsOrder.includes(draggableId)
-      ) {
+      if (!state.allLists[sourceListId].columnIdsOrder.includes(draggableId)) {
         throw new Error(
           'The draggableId is not a valid column id, it does not exist in allLists[sourceListId].columnIds. This should not happen',
         );
       }
 
       // Ready to update allLists
-      const [remove] = state.allLists[
-        sourceListId
-      ].columnIdsOrder.splice(source.index, 1);
-      state.allLists[sourceListId].columnIdsOrder.splice(
-        destination!.index,
-        0,
-        remove,
-      );
+      const [remove] = state.allLists[sourceListId].columnIdsOrder.splice(source.index, 1);
+      state.allLists[sourceListId].columnIdsOrder.splice(destination!.index, 0, remove);
     },
     toggleSidebar: (state: ListsState) => {
       state.isSidebarOpen = !state.isSidebarOpen;
