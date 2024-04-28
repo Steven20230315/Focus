ae4f9e5b3854836ea1283d2552548a43d79c4a1a
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-28 14:37:30 +0800

    Refactor Datepicker component for clearer date selection feedback
    This update enhances the user experience by adjusting the default visual
    cues associated with date selection in the Datepicker component.
    Previously, the datepicker incorrectly suggested that a date
    (today's date) was selected by default upon opening, which could confuse
    users. Now, the calendar icon and displayed date dynamically reflect
    whether a user has explicitly selected a date:
    
    - The `LuCalendarPlus` icon is shown when no date is selected,
    indicating the option to pick a date.
    - Upon opening the datepicker without a selection, "Pick a date" is
    displayed instead of today's date, clarifying that no date has been
    chosen yet.
    - Once a date is selected, the icon switches to `LuCalendar` and the
    selected date is displayed, providing clear visual feedback.
    
    These changes ensure that the calendar's UI more accurately represents the user's actions and decisions regarding date selection.

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -19,7 +19,7 @@ import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 export default function Datepicker() {
   const today = startOfDay(new Date());
-  const [selectedDay, setSelectedDay] = useState(today);
+  const [selectedDay, setSelectedDay] = useState<Date | null>(null);
   // State to control the open status of the menu
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -30,8 +30,36 @@ export default function Datepicker() {
 
   const [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'));
   const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date());
-  function classNames(...classes: (string | boolean | undefined)[]) {
-    return classes.filter(Boolean).join(' ');
+
+  // A helper function to compute the class name for the day button
+  function getDayButtonClass(day: Date, selectedDay: Date | null, today: Date, active: boolean) {
+    // The base class for every day button
+    const baseClass = 'mx-auto flex h-6 w-6 items-center justify-center rounded-full';
+    let classNames = baseClass;
+
+    // If user selected a day, check if the day is the same as the selected day
+    if (selectedDay && isSameDay(day, selectedDay)) {
+      // Check if the selected day is today
+      classNames += isToday(day) ? ' bg-sky-900 text-white' : ' bg-gray-900 text-white';
+    } else {
+      if (isToday(day)) {
+        classNames += ' bg-red-600 text-base font-normal text-white';
+      } else if (isSameMonth(day, today)) {
+        classNames += ' text-gray-900';
+      } else {
+        classNames += ' text-gray-600/90';
+      }
+    }
+
+    if (active && !selectedDay) {
+      classNames += ' bg-sky-100 text-sky-900';
+    }
+
+    if ((selectedDay && (isSameDay(day, selectedDay) || isToday(day))) || active) {
+      classNames += ' font-light';
+    }
+
+    return classNames;
   }
 
   const newDays = eachDayOfInterval({
@@ -54,110 +82,96 @@ export default function Datepicker() {
   }
 
   return (
-    <div className="min-h-400 flex justify-center ">
-      <Menu as="div" className="relative">
-        {({ open }) => (
-          <>
-            <Menu.Button onClick={toggleMenu} className={'flex items-center gap-4'}>
-              {open ? (
-                <>
-                  <LuCalendar />
-                  <div>{format(selectedDay, 'P')}</div>
-                </>
-              ) : (
-                <LuCalendarPlus className="h-5 w-5 text-white" />
-              )}
-            </Menu.Button>
-            <Transition
-              enter="transform transition duration-100 ease-in"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="transform duration-75 ease-out"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Menu.Items className="rounded-md bg-sky-600 px-4 py-2">
-                <div className="flex items-center">
-                  <h2 className="flex-auto text-sm font-semibold text-gray-900">
-                    {format(firstDayCurrentMonth, 'MMMM yyyy')}
-                  </h2>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        type="button"
-                        onClick={prevMonth}
-                        className={`-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 ${active ? 'text-gray-500' : 'text-gray-400'}`}
-                      >
-                        <span className="sr-only">Previous month</span>
-                        <FiChevronLeft className="h-5 w-5" aria-hidden="true" />
-                      </button>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        type="button"
-                        onClick={nextMonth}
-                        className={`-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 ${active ? 'text-gray-500' : 'text-gray-400'}`}
-                      >
-                        <span className="sr-only">Previous month</span>
-                        <FiChevronRight className="h-5 w-5" aria-hidden="true" />
-                      </button>
-                    )}
-                  </Menu.Item>
+    <Menu as="div" className="relative">
+      {({ open }) => (
+        <>
+          <input
+            type="text"
+            hidden
+            value={selectedDay ? format(selectedDay, 'yyyy-MM-dd') : undefined}
+            name="date"
+            readOnly
+          />
+          <Menu.Button onClick={toggleMenu} className={' flex items-center justify-center  gap-4'}>
+            {!open && selectedDay === null ? (
+              <LuCalendarPlus className="h-5 w-5 text-white" />
+            ) : (
+              <>
+                <LuCalendar className="h-5 w-5 text-white" />
+                <div>{selectedDay ? format(selectedDay, 'P') : 'Pick a date'}</div>
+              </>
+            )}
+          </Menu.Button>
+          <Transition
+            enter="transform transition duration-100 ease-in"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transform duration-75 ease-out"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute -right-28 mt-2 w-60 rounded-md bg-sky-600 px-4 py-2">
+              <div className="flex items-center">
+                <h2 className="flex-auto text-sm font-semibold text-gray-900">
+                  {format(firstDayCurrentMonth, 'MMMM yyyy')}
+                </h2>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      type="button"
+                      onClick={prevMonth}
+                      className={`-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 ${active ? 'text-gray-500' : 'text-gray-400'}`}
+                    >
+                      <span className="sr-only">Previous month</span>
+                      <FiChevronLeft className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      type="button"
+                      onClick={nextMonth}
+                      className={`-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 ${active ? 'text-gray-500' : 'text-gray-400'}`}
+                    >
+                      <span className="sr-only">Previous month</span>
+                      <FiChevronRight className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+              <div className=" grid md:divide-gray-200">
+                <div className="mt-3 grid grid-cols-7 text-center text-xs font-medium leading-6 text-gray-900">
+                  {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day, index) => (
+                    <div key={`day ${index}`}>{day}</div>
+                  ))}
                 </div>
-                <div className=" grid md:divide-gray-200">
-                  <div className="mt-3 grid grid-cols-7 text-center text-xs font-medium leading-6 text-gray-900">
-                    {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day, index) => (
-                      <div key={`day ${index}`}>{day}</div>
-                    ))}
-                  </div>
 
-                  <div className="mt-2 grid grid-cols-7 text-sm">
-                    {newDays.map((day, index) => (
-                      <Menu.Item
-                        as="div"
-                        onClick={(e) => e.preventDefault()}
-                        key={day.toString()}
-                        className={classNames(index > 6 && 'border-t border-gray-200', 'py-1.5')}
-                      >
-                        {({ active }) => (
-                          <button
-                            type="button"
-                            onClick={() => setSelectedDay(day)}
-                            // onClick={(e) => console.log(e)}
-                            className={`${classNames(
-                              isSameDay(day, selectedDay) && 'text-white',
-                              !isSameDay(day, selectedDay) &&
-                                isToday(day) &&
-                                'bg-red-600  text-base font-semibold text-white',
-                              !isSameDay(day, selectedDay) &&
-                                !isToday(day) &&
-                                isSameMonth(day, today) &&
-                                'text-gray-900',
-                              !isSameDay(day, selectedDay) &&
-                                !isToday(day) &&
-                                !isSameMonth(day, today) &&
-                                'text-gray-400',
-                              isSameDay(day, selectedDay) && isToday(day) && 'bg-sky-900',
-                              isSameDay(day, selectedDay) && !isToday(day) && 'bg-gray-900',
-                              !isSameDay(day, selectedDay) && active ? 'bg-sky-100 text-sky-900' : 'text-gray-900',
-                              (isSameDay(day, selectedDay) || isToday(day)) && 'font-light',
-                              'mx-auto flex h-8 w-8 items-center justify-center rounded-full ',
-                            )}`}
-                          >
-                            <time dateTime={format(day, 'yyyy-MM-dd')}>{format(day, 'd')}</time>
-                          </button>
-                        )}
-                      </Menu.Item>
-                    ))}
-                  </div>
+                <div className=" mt-2 grid grid-cols-7 text-sm">
+                  {newDays.map((day, index) => (
+                    <Menu.Item
+                      as="div"
+                      onClick={(e) => e.preventDefault()}
+                      key={day.toString()}
+                      className={index > 6 ? 'border-t border-gray-200 py-1.5' : 'py-1.5'}
+                    >
+                      {({ active }) => (
+                        <button
+                          type="button"
+                          onClick={() => setSelectedDay(day)}
+                          className={getDayButtonClass(day, selectedDay, today, active)}
+                        >
+                          <time dateTime={format(day, 'yyyy-MM-dd')}>{format(day, 'd')}</time>
+                        </button>
+                      )}
+                    </Menu.Item>
+                  ))}
                 </div>
-              </Menu.Items>
-            </Transition>
-          </>
-        )}
-      </Menu>
-    </div>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
   );
 }
