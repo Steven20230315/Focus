48c2bb6c74b5ba26ea20d46d9bd93d3257aa003e
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-29 15:49:39 +0800

    style: Improve code style consistency

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -7,14 +7,7 @@ import { FaArchive } from 'react-icons/fa';
 import { MdOutlineDriveFileMove } from 'react-icons/md';
 import { FaRegShareSquare } from 'react-icons/fa';
 
-export type MenuOptions =
-  | 'Edit'
-  | 'Duplicate'
-  | 'Delete'
-  | 'Favorite'
-  | 'Move'
-  | 'Share'
-  | 'Archive';
+export type MenuOptions = 'Edit' | 'Duplicate' | 'Delete' | 'Favorite' | 'Move' | 'Share' | 'Archive';
 
 const icons = {
   Edit: FiEdit3,
@@ -32,11 +25,7 @@ type MenuItemProps = {
   onClick?: () => void;
 };
 
-export default function MenuItem({
-  type,
-  text,
-  onClick,
-}: MenuItemProps) {
+export default function MenuItem({ type, text, onClick }: MenuItemProps) {
   const Icon = icons[type];
   return (
     <div className="py-1" onClick={onClick}>
@@ -44,18 +33,9 @@ export default function MenuItem({
         {({ active }) => (
           <a
             href="#"
-            className={`flex items-center px-4 py-2 text-sm ${
-              active
-                ? 'bg-indigo-500 text-white'
-                : 'text-gray-700'
-            }`}
+            className={`flex items-center px-4 py-2 text-sm ${active ? 'bg-indigo-500 text-white' : 'text-gray-700'}`}
           >
-            {Icon && (
-              <Icon
-                className="mr-3 h-5 w-5"
-                aria-hidden="true"
-              />
-            )}
+            {Icon && <Icon className="mr-3 h-5 w-5" aria-hidden="true" />}
             {text}
           </a>
         )}
--- src/features/list/listSelector.ts
+++ src/features/list/listSelector.ts
@@ -2,24 +2,17 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../../store';
 import type { List, ListId } from '../../types';
 
-export const selectCurrentListId = (state: RootState) =>
-  state.list.currentListId;
+export const selectCurrentListId = (state: RootState) => state.list.currentListId;
 
-export const selectAllLists = (state: RootState) =>
-  state.list.allLists;
+export const selectAllLists = (state: RootState) => state.list.allLists;
 
-export const selectAllListIDsInOrder = (state: RootState) =>
-  state.list.listsOrder;
+export const selectAllListIDsInOrder = (state: RootState) => state.list.listsOrder;
 
-export const currentListDetails = (state: RootState) =>
-  state.list.allLists[state.list.currentListId];
+export const currentListDetails = (state: RootState) => state.list.allLists[state.list.currentListId];
 
 export const getCurrentListDetails = createSelector(
   [selectAllLists, selectCurrentListId],
-  (
-    allList: Record<ListId, List>,
-    currentListId: ListId,
-  ) => {
+  (allList: Record<ListId, List>, currentListId: ListId) => {
     return {
       listTitle: allList[currentListId].title,
       listId: allList[currentListId].listId,
@@ -29,10 +22,7 @@ export const getCurrentListDetails = createSelector(
 
 export const getCurrentListIdAndTitle = createSelector(
   [selectAllLists, selectCurrentListId],
-  (
-    allList: Record<ListId, List>,
-    currentListId: ListId,
-  ) => {
+  (allList: Record<ListId, List>, currentListId: ListId) => {
     // This will be reevaluated when allList or currentListId changes
 
     return {
