5e8d041954a127a77e1df6cdab0e80e12dddd0f4
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-29 15:50:40 +0800

    feat: Enhance Sidebar component with toggle feature

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,21 @@
-import styled from "styled-components";
-import List from "../features/list/List";
-import CreateList from "../features/list/CreateList";
-
-const Container = styled.div``;
-
+import LinkList from '../features/list/LinkList';
+import CreateList from '../features/list/CreateList';
+import Checkbox from './Checkbox';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSidebar } from '../features/list/listSlice';
+import { RootState } from '../store';
 export default function Sidebar() {
+  const dispatch = useDispatch();
+  const isSidebarOpen = useSelector((state: RootState) => state.list.isSidebarOpen);
   return (
-    <Container className="flex flex-col gap-2 bg-slate-100 p-2">
-      <List />
+    <div
+      className={`sticky top-0 flex min-h-screen min-w-[250px] flex-col  px-2 pt-4  ${!isSidebarOpen ? 'bg-gray-700' : 'border border-gray-500 bg-gray-600 shadow-xl shadow-black'}`}
+    >
+      <div className="mb-4 flex justify-end">
+        <Checkbox onClick={() => dispatch(toggleSidebar())} id="toggle sidebar" />
+      </div>
+      <LinkList />
       <CreateList />
-    </Container>
+    </div>
   );
 }
