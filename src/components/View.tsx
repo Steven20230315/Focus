8550c3388121890e69fd23708ee82e01f5a576f9
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-29 15:56:34 +0800

    refactor: Update View component to use Tailwind

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,30 +1,28 @@
-import styled from "styled-components";
-import { useSelector } from "react-redux";
-import CreateTask from "../features/task/CreateTask";
-import { getCurrentListIdAndTitle } from "../features/list/listSelector";
-import ColumnList from "../features/column/ColumnList";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  .display {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 6rem;
-    border: 1px solid black;
-  }
-`;
+import { useSelector } from 'react-redux';
+import ColumnList from '../features/column/ColumnList';
+import { Transition } from '@headlessui/react';
+import { selectCurrentListDetails } from '../features/list/listSelector';
 
 export default function View() {
   // The view component only receives the list id and list title of the current list.
   // Changes to the columnIds or columnIdsOrder of the current list will not cause a re-render
-  const { listTitle } = useSelector(getCurrentListIdAndTitle);
+
+  const { title } = useSelector(selectCurrentListDetails);
   return (
-    <Container className="container col-span-3 bg-stone-100">
-      view
-      <h2>{listTitle}</h2>
-      <CreateTask />
-      <ColumnList />
-    </Container>
+    <Transition
+      show={true}
+      id="sidebar"
+      as="div"
+      enter="transition ease-in duration-300"
+      enterFrom="transform -translate-x-full opacity-0"
+      enterTo="transform translate-x-0 opacity-100"
+      leave="transition ease-in duration-400"
+      leaveFrom="transform translate-x-0 opacity-100"
+      leaveTo="transform -translate-x-full opacity-0"
+      className="item-center relative flex  grow flex-col gap-6  p-6"
+    >
+      <h2 className="text-center text-7xl font-bold"> {title ? title : 'Create a list to get started!'}</h2>
+      {title && <ColumnList />}
+    </Transition>
   );
 }
