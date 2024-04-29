e2f2a26ecba25f336c9ecaadc5c002c437b43413
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-29 14:41:08 +0800

    feat: Improve interactivity of CreateList
    - Integrate `useCloseOnLoseFocus` and `useEscapeClose` custom hooks to
    managing closing the CreatList component on outside clicks and Escape
    key presses.
    
    - Refactored the form submission logic to include validation against
    existing list titles, preventing duplicate lists and providing immediate
    feedback to the user.

--- src/features/list/CreateList.tsx
+++ src/features/list/CreateList.tsx
@@ -1,40 +1,70 @@
-import { type FormEvent } from 'react';
+import { type FormEvent, useState, useRef, useEffect } from 'react';
 import { createNewListWithDefaultColumns } from '../../utils/createNewList';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addListWithDefaultColumns } from './listSlice';
-import styled from 'styled-components';
-
-const Container = styled.form`
-	display: flex;
-	flex-direction: column;
-	padding: 0.5rem;
-	gap: 0.5rem;
-	border: 1px solid black;
-`;
+import { selectAllListTitles } from './listSelector';
+import useCloseOnLoseFocus from '../../hooks/useCloseOnLoseFocus';
+import useEscapeClose from '../../hooks/useEscapeClose';
 
 export default function CreateList() {
-	const dispatch = useDispatch();
-	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+  const dispatch = useDispatch();
+  const ref = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const listTitles = useSelector(selectAllListTitles);
+  const [title, setTitle] = useState('');
+  const [labelMessage, setLabelMessage] = useState('Enter list title');
+  useEffect(() => {
+    const listExists = listTitles.includes(title);
+    setLabelMessage(listExists ? 'List already exists' : 'Enter list title');
+  }, [listTitles, title]);
 
-		const title = new FormData(e.currentTarget).get('title') as string;
-		const trimTitle = title.trim();
-		if (!trimTitle) {
-			console.log('Please enter a title');
-			return;
-		} else {
-			// create a new list with default columns
-			const newList = createNewListWithDefaultColumns(trimTitle);
-			// Pass the new list to
-			dispatch(addListWithDefaultColumns(newList));
-		}
-		e.currentTarget.reset();
-	};
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-	return (
-		<Container onSubmit={onSubmit} autoComplete='off'>
-			<input type='text' name='title' />
-			<button>Add new list</button>
-		</Container>
-	);
+    const trimTitle = title.trim();
+    if (!trimTitle) {
+      console.log('Please enter a title');
+      return;
+    }
+    if (listTitles.includes(trimTitle)) {
+      setLabelMessage('List already exists');
+      return;
+    }
+    dispatch(addListWithDefaultColumns(createNewListWithDefaultColumns(trimTitle)));
+    setTitle(''); // Reset title state instead of using form reset
+    setIsOpen(false);
+  };
+
+  useCloseOnLoseFocus(ref, isOpen, setIsOpen);
+  useEscapeClose(isOpen, setIsOpen);
+
+  return (
+    <div className="mt-4 cursor-pointer" ref={ref}>
+      {!isOpen ? (
+        <button type="button" onClick={() => setIsOpen(true)} className="ml-2">
+          + Create List
+        </button>
+      ) : (
+        <form onSubmit={onSubmit} autoComplete="off" className="mt-4">
+          {/* To position label  */}
+          <div className="relative">
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="peer rounded-md px-2 py-1 text-slate-600 focus:outline-none"
+            />
+            <label
+              htmlFor="title"
+              className="absolute left-0 top-0 px-2 py-1 text-slate-600 transition-all peer-placeholder-shown:text-gray-400 peer-focus:-top-7 peer-focus:text-gray-300 "
+            >
+              {labelMessage}
+            </label>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
