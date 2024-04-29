629b6972ff6beb5963b48e4aae71754245c27e05
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-04-29 18:01:46 +0800

    Refactor TaskItem and TaskList to improve data handling
    - Modify TaskItem to receive a complete Task object instead of just a
    TaskId.
    - Update TaskList to use selectTasksInColumn for fetching task details
    directly, reducing the complexity of passing and managing individual
    task IDs.
    - Simplify grid structure in TaskList and remove unused imports to
    clean up the codebase.

--- src/features/task/TaskItem.tsx
+++ src/features/task/TaskItem.tsx
@@ -1,34 +1,45 @@
-import { TaskId } from '../../types';
-import { useSelector } from 'react-redux';
-import type { RootState } from '../../store';
+import { Task } from '../../types';
+import { useDispatch } from 'react-redux';
 import TaskTitle from './TaskTitle';
 import { Draggable, type DraggableProvided } from '@hello-pangea/dnd';
-import { AiOutlineSmallDash } from 'react-icons/ai';
+// import { AiOutlineSmallDash } from 'react-icons/ai';
 import Datepicker from '../../components/Datepicker';
+import { updateTask, type UpdateTaskPayload } from './taskSlice';
+import { format } from 'date-fns';
 
 type TaskItemProps = {
-  taskId: TaskId;
+  task: Task;
   index: number;
 };
 
-export default function TaskItem({ taskId, index }: TaskItemProps) {
-  const task = useSelector((state: RootState) => state.task.allTasks[taskId]);
+export default function TaskItem({ task, index }: TaskItemProps) {
+  const dispatch = useDispatch();
+
+  const updateDate = (day: Date) => {
+    dispatch(
+      updateTask({
+        taskId: task.taskId,
+        dueDate: format(day, 'yyyy-MM-dd'),
+      } as UpdateTaskPayload),
+    );
+  };
+
   return (
-    <Draggable draggableId={taskId} index={index}>
+    <Draggable draggableId={task.taskId} index={index}>
       {(provided: DraggableProvided) => (
         <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
           <div className="my-1 grid h-full cursor-move grid-cols-12 content-center items-center gap-4 rounded-md py-2 text-sm hover:bg-slate-300 ">
             {/* <AiOutlineCheckCircle/> */}
             <div className="col-span-5">
-              <TaskTitle taskId={taskId} title={task.title} columnId={task.columnId} />
+              <TaskTitle taskId={task.taskId} title={task.title} columnId={task.columnId} />
             </div>
-            <div className="col-span-7 col-start-6 grid grid-cols-6 place-items-center gap-6 text-center">
+            <div className="col-span-6 col-start-6 grid grid-cols-6 place-items-center gap-6 text-center">
               <div className="col-span-2">Time Spend</div>
-              <div className="col-span-2">{task.dueDate ? task.dueDate : <Datepicker />}</div>
-              <div className="col-span-1">{task.priority}</div>
-              <div>
+              <div className="col-span-2">{task.dueDate ? task.dueDate : <Datepicker onDateSelect={updateDate} />}</div>
+              <div className="col-span-2">{task.priority}</div>
+              {/* <div>
                 <AiOutlineSmallDash className="m-auto" />
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
--- src/features/task/TaskList.tsx
+++ src/features/task/TaskList.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import type { ColumnId, TaskId } from '../../types';
+import type { ColumnId, Task } from '../../types';
 import TaskItem from './TaskItem';
 import { useSelector } from 'react-redux';
-import type { RootState } from '../../store';
 import { Droppable, type DroppableProvided } from '@hello-pangea/dnd';
-import { FiPlusCircle } from 'react-icons/fi';
+import { selectTasksInColumn } from './TaskSelector';
+// import { FiPlusCircle } from 'react-icons/fi';
 
 type TaskListProps = React.HTMLAttributes<HTMLDivElement> & {
   columnId: ColumnId;
@@ -13,20 +13,19 @@ type TaskListProps = React.HTMLAttributes<HTMLDivElement> & {
 };
 
 export default function TaskList({ columnId, onMouseEnter, onMouseLeave }: TaskListProps) {
-  const tasksInColumn = useSelector((state: RootState) => state.column.allColumns[columnId].taskIds);
-
+  const tasks = useSelector(selectTasksInColumn(columnId));
   return (
     <>
       <div className="divide-y-2" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-        <div className=" grid grid-cols-12 gap-4 py-2 text-start text-lg font-bold">
+        <div className=" grid h-fit grid-cols-12 gap-4  py-2 text-start text-lg font-bold sm:text-xs md:text-base">
           <div className="col-span-5">name</div>
-          <div className="col-span-7 col-start-6 grid grid-cols-6 place-items-center gap-6 text-center">
+          <div className="col-span-6 col-start-6 grid grid-cols-6 place-items-center gap-6 text-center">
             <div className="col-span-2">Time Spend</div>
             <div className="col-span-2">Due Date</div>
-            <div className="col-span-1">Priority</div>
-            <div>
+            <div className="col-span-2">Priority</div>
+            {/* <div>
               <FiPlusCircle />
-            </div>
+            </div> */}
           </div>
         </div>
         <Droppable droppableId={columnId} type="task">
@@ -36,8 +35,8 @@ export default function TaskList({ columnId, onMouseEnter, onMouseLeave }: TaskL
               {...provided.droppableProps}
               className="mb-3 flex flex-col justify-center divide-y"
             >
-              {tasksInColumn.map((taskId: TaskId, index: number) => (
-                <TaskItem key={taskId} taskId={taskId} index={index} />
+              {tasks.map((task: Task, index: number) => (
+                <TaskItem key={task.taskId} task={task} index={index} />
               ))}
               {provided.placeholder}
             </div>
