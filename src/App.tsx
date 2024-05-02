231d4320126cf7a65c05f1cac7f05f1967eda3e5
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-05-02 20:04:52 +0800

    refactor: update main application structure for dynamic sidebar and timer
    - Integrate Redux state management to handle sidebar visibility and
    timer activity dynamically within the `App` component.
    - Remove unused `MainNav` component import and adjust layout to
    accommodate sidebar's conditional rendering and timer display.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import MainNav from "./components/MainNav";
-import Sidebar from "./components/Sidebar";
-import View from "./components/View";
-import { DragDropContext } from "@hello-pangea/dnd";
-import { useDragDrop } from "./hooks/useDragDrop";
+// import MainNav from "./components/MainNav";
+import { useSelector } from 'react-redux';
+import { RootState } from './store';
+import Sidebar from './components/Sidebar';
+import View from './components/View';
+import { DragDropContext } from '@hello-pangea/dnd';
+import { useDragDrop } from './hooks/useDragDrop';
+import Timer from './features/timer/Timer';
+
 function App() {
   const onDragEnd = useDragDrop();
+  const isSidebarOpen = useSelector((state: RootState) => state.list.isSidebarOpen);
+  const isTimerActive = useSelector((state: RootState) => state.timer.isActive);
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div className="h-screen w-full bg-slate-200 ">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <MainNav />
-        <div className="grid h-screen grid-cols-4 gap-5 ">
+      <div className="flex">
+        <div
+          className={`${!isSidebarOpen && '-ml-[185px]'} sticky top-0 z-10 flex h-screen w-[250px] justify-between gap-6 bg-stone-200 font-mono transition-all duration-500`}
+        >
           <Sidebar />
+        </div>
+        <div className="grow px-6 ">
           <View />
         </div>
       </div>
+      {isTimerActive && <Timer />}
     </DragDropContext>
   );
 }
