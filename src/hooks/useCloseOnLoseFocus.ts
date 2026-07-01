488be54db387518626c264a930e2cc9efa841b52
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2026-07-01 10:39:49 +0800

    fix(hooks): allow nullable refs in focus loss handler

--- src/hooks/useCloseOnLoseFocus.ts
+++ src/hooks/useCloseOnLoseFocus.ts
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
 
-function useCloseOnLoseFocus(ref: React.RefObject<HTMLElement>, isOpen: boolean, setIsOpen: (isOpen: boolean) => void) {
+function useCloseOnLoseFocus(
+  ref: React.RefObject<HTMLElement | null>,
+  isOpen: boolean,
+  setIsOpen: (isOpen: boolean) => void,
+) {
   useEffect(() => {
     function handleClickOutside(e: MouseEvent) {
-      if (ref.current && !ref.current.contains(e.target as Node)) {
+      if (ref?.current && !ref.current.contains(e.target as Node)) {
         setIsOpen(false);
       }
     }
